bb7bc8fc84c478cc3e04a62708c9d88fe29c99aa
Justas Palumickas <jpalumickas@gmail.com>
2019-12-13 17:28:52 +0200

    Add general matchers

---
+++ src/matchers.js
@@ -0,0 +1,18 @@
+import { darken, ligthen, invert } from 'polished';
+
+const toPercent = (val) => parseInt(val, 10) / 100;
+
+export default [
+  {
+    regex: 'darken(\\d+)',
+    resolve: ({ value, matches }) =>  darken(toPercent(matches[1]), value),
+  },
+  {
+    regex: 'lighten(\\d+)',
+    resolve: ({ value, matches }) => lighten(toPercent(matches[1]), value),
+  },
+  {
+    regex: 'invert',
+    resolve: ({ value, matches }) => invert(value),
+  },
+]
